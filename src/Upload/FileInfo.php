c99fe82d68174fa5e85295ab714b130f548d332a
Wim Verstuyf <wim.verstuyf@eenvoudigfactureren.be>
2024-03-06 11:40:12 +0100

    fix for php8.1

--- src/Upload/FileInfo.php
+++ src/Upload/FileInfo.php
@@ -119,7 +119,7 @@ class FileInfo extends \SplFileInfo implements \Upload\FileInfoInterface
      *
      * @return string
      */
-    public function getExtension()
+    public function getExtension(): string
     {
         return $this->extension;
     }
